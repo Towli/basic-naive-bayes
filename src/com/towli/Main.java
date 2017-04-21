80b4b3b80a78da2ca753cb1c75f9b75228e9658a
Alex Towli <alextowli@hotmail.co.uk>
2017-04-21 16:14:33 +0100

    begin building the driver class - add function to read in data (instances) from a given .arff file

--- src/com/towli/Main.java
+++ src/com/towli/Main.java
@@ -1,8 +1,37 @@
 package com.towli;
 
+import weka.core.Instances;
+
+import java.io.FileReader;
+
+/**
+ * Driver class for processing BasicNaiveBayes experiments
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Read in .arff files
+        String filePath = "data/crime_train.arff";
+        Instances trainingInstances = readData(filePath);
+
+        // getInstances() from .arffs
+        // Create new BasicNaiveBayes classifier
+        // Classify the test data
     }
+
+    /**
+     * Get Instances from specified data file
+     * @return Instances
+     */
+    public static Instances readData(String filePath) {
+        Instances instances = null;
+        try {
+            FileReader reader = new FileReader(filePath);
+            instances = new Instances(reader);
+        } catch(Exception e) {
+            System.out.println("Exception caught: "+e);
+        }
+        return instances;
+    }
+
 }
