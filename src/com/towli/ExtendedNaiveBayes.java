9a06dbd2657fbef3cf452c93b030f64c04837508
Alex Towli <alextowli@hotmail.co.uk>
2017-05-01 21:09:45 +0100

    create skeleton Extended Naive Bayes class

---
+++ src/com/towli/ExtendedNaiveBayes.java
@@ -0,0 +1,10 @@
+package com.towli;
+
+/**
+ * Created by Towli on 01/05/2017.
+ */
+public class ExtendedNaiveBayes extends BasicNaiveBayes{
+    public ExtendedNaiveBayes() {
+        super();
+    }
+}
